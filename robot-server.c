eb97757def59d42fddb9190a1a6be19c2547f969
Martin <32516410+Xerenz@users.noreply.github.com>
2019-11-19 22:52:24 +0530

    I was never here

---
+++ robot-server.c
@@ -0,0 +1,166 @@
+/*
+*****************************************************************************************
+*
+*        		===============================================
+*           		Rapid Rescuer (RR) Theme (eYRC 2019-20)
+*        		===============================================
+*
+*  This script is to implement Task 1B of Rapid Rescuer (RR) Theme (eYRC 2019-20).
+*  
+*  This software is made available on an "AS IS WHERE IS BASIS".
+*  Licensee/end user indemnifies and will keep e-Yantra indemnified from
+*  any and all claim(s) that emanate from the use of the Software or 
+*  breach of the terms of this agreement.
+*  
+*  e-Yantra - An MHRD project under National Mission on Education using ICT (NMEICT)
+*
+*****************************************************************************************
+*/
+
+/*
+# Team ID:			[ 3478 ]
+# Author List:		[ Jyothis P, Arun Padmanabhan, Ebin Santy, Jithin K Satheesh  ]
+* Filename:			task_1a.py
+* Functions:		readImage, solveMaze
+* 					[ Comma separated list of functions in this file ]
+* Global variables:	CELL_SIZE
+* 					[ List of global variables defined in this file ]
+*/
+
+
+// Include necessary header files
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h> 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+#include <arpa/inet.h>
+
+
+// Constants defined
+#define SERVER_PORT 3333
+#define RX_BUFFER_SIZE 1024
+#define TX_BUFFER_SIZE 1024
+
+#define MAXCHAR 1000				// max characters to read from txt file
+
+// Global variables
+struct sockaddr_in dest_addr;
+struct sockaddr_in source_addr;
+
+char rx_buffer[RX_BUFFER_SIZE];		// buffer to store data from client
+char tx_buffer[RX_BUFFER_SIZE];		// buffer to store data to be sent to client
+
+char ipv4_addr_str[128];			// buffer to store IPv4 addresses as string
+char ipv4_addr_str_client[128];		// buffer to store IPv4 addresses as string
+
+int listen_sock;
+
+char line_data[MAXCHAR];
+
+FILE *input_fp, *output_fp;
+
+
+/*
+* Function Name:	socket_create
+* Inputs:			dest_addr [ structure type for destination address ]
+* 					source_addr [ structure type for source address ]
+* Outputs: 			my_sock [ socket value, if connection is properly created ]
+* Purpose: 			the function creates the socket connection with the server
+* Example call: 	int sock = socket_create(dest_addr, source_addr);
+*/
+int socket_create(struct sockaddr_in dest_addr, struct sockaddr_in source_addr){
+
+	int addr_family;
+	int ip_protocol;
+	int opt = 1;
+	int new_socket;
+	dest_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	dest_addr.sin_family = AF_INET;
+	dest_addr.sin_port = htons(SERVER_PORT);
+	addr_family = AF_INET;
+
+    int addrlen = sizeof(dest_addr); 
+	ip_protocol = IPPROTO_IP;
+
+	int my_sock;
+
+	my_sock = socket(addr_family, SOCK_STREAM, ip_protocol);
+
+	setsockopt(my_sock, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
+	bind(my_sock, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
+	listen(my_sock, 3);
+	printf("Server running at 3333\n");
+	new_socket = accept(my_sock, (struct sockaddr *)&dest_addr, (socklen_t*)&addrlen);
+	printf("Connection established with the client.\n");
+	return new_socket;
+}
+
+
+/*
+* Function Name:	receive_from_send_to_client
+* Inputs:			sock [ socket value, if connection is properly created ]
+* Outputs: 			None
+* Purpose: 			the function receives the data from server and updates the 'rx_buffer'
+*					variable with it, sends the obstacle position based on obstacle_pos.txt
+*					file and sends this information to the client in the provided format.
+* Example call: 	receive_from_send_to_client(sock);
+*/
+int receive_from_send_to_client(int sock){
+
+    char *hello = "@(4,7)@"; 
+    int valread;
+    valread = read( sock , rx_buffer, RX_BUFFER_SIZE); 
+	// printf("%d\n",valread ); 				
+    if (valread > 0)
+    {
+    	printf("%s\n", rx_buffer);
+	    send(sock , hello , strlen(hello) , 0 ); 
+	    printf("obstacle sent\n");    	
+    }
+	return 0;
+
+}
+
+
+int main() {
+	
+    char *input_file_name = "obstacle_pos.txt";
+	char *output_file_name = "data_from_client.txt";
+
+	// Create socket and accept connection from client
+	int sock = socket_create(dest_addr, source_addr);
+
+	input_fp = fopen(input_file_name, "r");
+
+	if (input_fp == NULL){
+		printf("Could not open file %s\n",input_file_name);
+		return 1;
+	}
+
+	fgets(line_data, MAXCHAR, input_fp);
+
+	output_fp = fopen(output_file_name, "w");
+
+	if (output_fp == NULL){
+		printf("Could not open file %s\n",output_file_name);
+		return 1;
+	}
+
+	while (1) {
+
+		// Receive and send data from client and get the new shortest path
+		receive_from_send_to_client(sock);
+
+		// NOTE: YOU ARE NOT ALLOWED TO MAKE ANY CHANGE HERE
+		fputs(rx_buffer, output_fp);
+		fputs("\n", output_fp);
+
+	}
+
+	return 0;
+}
+
